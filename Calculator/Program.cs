b55006305fe9f3e785f1299f7f8e2d07a999cf19
XRicko <idims52@gmail.com>
2019-10-06 22:30:18 +0300

    create

---
+++ Calculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char operation;
+            double number;
+            double result = 0;
+
+            Console.WriteLine("Welcome to the \"very\" useful calculator!");
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Choose operation(+, -, *, /, = to show result and stop): ");
+                    operation = Convert.ToChar(Console.ReadLine());
+
+                    if (operation == '=')
+                    {
+                        Console.WriteLine(result);
+                        Console.ReadKey();
+                        break;
+                    }
+
+                    Console.Write("Enter a number: ");
+                    number = Convert.ToDouble(Console.ReadLine());
+
+                    switch (operation)
+                    {
+                        case '+':
+                            result += number;
+                            break;
+                        case '-':
+                            result -= number;
+                            break;
+                        case '*':
+                            result *= number;
+                            break;
+                        case '/':
+                            result /= number;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid operation");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
